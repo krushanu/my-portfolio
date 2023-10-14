df0b69097b0e133c7b9f58497f8e08aa4513af23
Krushanu Mohapatra <mohapatrakrushanu@gmail.com>
2023-10-14 01:27:08 -0400

    Flag code

--- app/components/CountryFlags.tsx
+++ app/components/CountryFlags.tsx
@@ -1,41 +1,47 @@
 import React from "react";
 
-// import {
-//   India,
-//   Singapore,
-//   Germany,
-//   Spain,
-//   Portugal,
-//   Switzerland,
-//   Malaysia,
-//   Indonesia,
-//   UnitedKingdom,
-//   Canada,
-// } from "@/app/components/svg";
-
-// import Canada from "./svg/Canada.svg";
+import India from "@/public/India.svg";
+import Singapore from "@/public/Singapore.svg";
+import Germany from "@/public/Germany.svg";
+import Spain from "@/public/Spain.svg";
+import Portugal from "@/public/Portugal.svg";
+import Switzerland from "@/public/Switzerland.svg";
+import Malaysia from "@/public/Malaysia.svg";
+import Indonesia from "@/public/Indonesia.svg";
+import UnitedKingdom from "@/public/UnitedKingdom.svg";
+import Canada from "@/public/Canada.svg";
 
 import CountryList from "./CountryList";
-import Image from "next/image";
 
-// const CountrySourceCode = [
-//   { src: India, code: "IN" },
-//   { src: Singapore, code: "SG" },
-//   { src: Germany, code: "DE" },
-//   { src: Spain, code: "ES" },
-//   { src: Portugal, code: "PT" },
-//   { src: Switzerland, code: "CH" },
-//   { src: Malaysia, code: "MY" },
-//   { src: Indonesia, code: "ID" },
-//   { src: UnitedKingdom, code: "GB" },
-//   { src: Canada, code: "CA" },
-// ];
+const CountrySourceCode = [
+  { src: India, code: "IN" },
+  { src: Singapore, code: "SG" },
+  { src: Germany, code: "DE" },
+  { src: Spain, code: "ES" },
+  { src: Portugal, code: "PT" },
+  { src: Switzerland, code: "CH" },
+  { src: Malaysia, code: "MY" },
+  { src: Indonesia, code: "ID" },
+  { src: UnitedKingdom, code: "GB" },
+  { src: Canada, code: "CA" },
+];
 
 const CountryFlags = () => {
   return (
     <ul className="justify-center space-x-4">
+      {CountrySourceCode.map((eachItem) => (
+        <CountryList
+          key={eachItem.src}
+          src={eachItem.src}
+          code={eachItem.code}
+        />
+      ))}
+
       {/* {CountrySourceCode.map((eachItem, index) => (
-        <CountryList key={index} src={eachItem.src} code={eachItem.code} />
+        <li key={index} className="tooltip tooltip-bottom text-xl" data-tip={eachItem.src}>
+          <Image loading="lazy" className='w-10' src={eachItem.src} alt={eachItem.code} />
+          <span hidden >{eachItem.src}</span>
+        </li>
       ))} */}
     </ul>
   );
