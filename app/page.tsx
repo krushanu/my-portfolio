19412c609a11409c0a8831087a0e023e76f3256e
Krushanu Mohapatra <mohapatrakrushanu@gmail.com>
2023-10-14 06:54:28 -0400

    Home page theme change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomeContent from "./components/HomeContent";
 
 export default function Home() {
   return (
-    <main className="w-full" data-theme="emerald">
+    <main className="w-full" data-theme="retro">
       <div className="grid grid-cols-1 lg:grid-cols-2 ">
         <HomeImage />
         <HomeContent />
