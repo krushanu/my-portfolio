3bdbf047f698947fc46373ebed55a44a7042578a
Krushanu Mohapatra <mohapatrakrushanu@gmail.com>
2024-03-04 00:45:54 -0500

    Updated schema

--- app/emailSchema.ts
+++ app/emailSchema.ts
@@ -1,27 +1,13 @@
 import { z } from "zod";
 
 const emailSchema = z.object({
-  name: z
-    .string({
-      required_error: "Name is required",
-      invalid_type_error: "Name must be a string",
-    })
-    .max(32),
-  email: z
-    .string({
-      required_error: "Email is required",
-      invalid_type_error: "Please enter a valid and complete email ID",
-    })
-    .email(),
+  name: z.string().min(1, "Name is required").max(32),
+  email: z.string().min(1, "Email is required").email(),
   number: z
-    .number({
-      invalid_type_error:
-        "Please enter a valid contact number in the form of +xxxxxxxxxxx ",
-    })
+    .number()
+    .min(1, "Please enter a valid contact number in the form of +xxxxxxxxxxx ")
     .max(99999999999),
-  message: z.string({
-    required_error: "Message is required",
-  }),
+  message: z.string().min(1, "Please enter a valid message ").max(99999999999),
 });
 
 export default emailSchema;
